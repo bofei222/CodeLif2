7d93361bb6a3cbb1a8354bfbb074053e86b2b78d
Sany_bof <bof@sany.com.cn>
2020-09-10 18:34:19 +0800

    sftp tdengine-demo

---
+++ SpringBoot-Learning-bf/springboot-tdengine-demo/src/main/java/com/bf/springboottdenginedemo/SpringbootTdengineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bf.springboottdenginedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTdengineDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTdengineDemoApplication.class, args);
+    }
+
+}
---
+++ SpringBoot-Learning-bf/springboot-tdengine-demo/src/test/java/com/bf/springboottdenginedemo/SpringbootTdengineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bf.springboottdenginedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTdengineDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
